b4af02acd540b7e3a8d2365e3dc89dbd707f370e
kubrakismet <58142308+kubrakismet@users.noreply.github.com>
2020-11-25 01:05:33 +0300

    Add files via upload

---
+++ pets/pets/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace pets
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Yönetici E-mail :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yönetici Şifresi :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(323, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GİRİŞ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(149, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 149);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ pets/pets/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace pets
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-SJ08IUG\MSSQLSERVER13;Initial Catalog=Pets;Integrated Security=True");
+
+        public bool YasakliKelime(string kullanici)
+        {
+            string[] liste = kullanici.Split(' ');
+            string[] yasaklikelime = { "where", "from", "delete", "drop", "alter table", "insert into", "update", "set", "join", "script", "body", "alert", "insert", "or", "set", "1","^","'"};
+            for (int i = 0; i < liste.Length; i++)
+            {
+                for (int j = 0; j < yasaklikelime.Length; j++)
+                {
+                    if (liste[i] == yasaklikelime[j])
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool kontrol = YasakliKelime(textBox1.Text + " " + textBox2.Text);
+            if (kontrol)
+            {
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("select*from Yonetici where Email='" + textBox1.Text + "'and Sifre='" + textBox2.Text + "'", baglanti);
+                SqlDataReader oku = komut.ExecuteReader();
+                if (oku.Read())
+                {
+                    rapor roku = new rapor();
+                    roku.Show();
+                }
+                else
+                  MessageBox.Show("Kullanıcı adı ve/veya şifre yanlış"); 
+            }
+            else
+              MessageBox.Show("kullanıcı adınızda/şifrenizde yasaklı kelime(ler) kullanmayınız.");
+            baglanti.Close();
+        }
+
+    }
+}
